3edad17eb30f7d169a902e86b7369c443ceae821
bibek-056 <115538325+bibek-056@users.noreply.github.com>
2023-01-13 21:20:38 +0545

    Create WorkoutContext.js

---
+++ frontend/src/context/WorkoutContext.js
@@ -0,0 +1,31 @@
+import { useReducer } from 'react'
+import { createContext } from 'react'
+
+export const WorkoutsContext = createContext()
+
+export const workoutsReducer = (state, action) => {
+    switch (action.type) {
+        case 'SET_WORKOUTS':
+            return{
+                workouts: action.payload
+            }
+        case 'CREATE_WORKOUT':
+            return {
+                workouts: [action.payload, ...state.workouts]
+            }
+        default:
+            return state
+    }
+}
+
+export const WorkoutsContextProvider = ({ childern }) => {
+    const[state, dispatch] = useReducer(workoutsReducer, {
+        workouts: null
+    })
+
+    return (
+        <WorkoutsContext.Provider value={{...state, dispatch}}>
+            { childern }
+        </WorkoutsContext.Provider>
+    )
+}
