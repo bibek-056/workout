489d03404a0eab39d4ec25c1b0a2aa905690dfc7
bibek-056 <115538325+bibek-056@users.noreply.github.com>
2023-01-13 21:22:43 +0545

    Create Home.js

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,36 @@
+import { useEffect } from 'react'
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
+import WorkoutDetails from '../components/WorkoutDetails'
+import WorkoutForm from '../components/WorkoutForm'
+
+const Home = () => {
+    // const [workouts, setWorkouts] = useState(null)
+    const { workouts, dispatch} = useWorkoutsContext()
+
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const response = await fetch('/api/workouts')
+            const json = await response.json()
+            
+            if (response.ok) {
+                // setWorkouts(json)
+                dispatch({type: 'SET_WORKOUTS', payload: json})
+            }
+        }
+
+        fetchWorkouts() 
+    }, [])
+
+    return(
+        <div className="home">
+            <div className="workouts">
+                {workouts && workouts.map((workout) => (
+                    <WorkoutDetails key={workout._id} workout={workout} /> 
+                ))}
+            </div>
+            <WorkoutForm />
+        </div>
+    )
+}
+
+export default Home
